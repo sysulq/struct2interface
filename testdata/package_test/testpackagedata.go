b8410aa549d8639739e0457d44faa901f50d0d66
x1nchen <ccx.opt@gmail.com>
2022-05-24 10:48:16 +0800

    test: fix the testcases

--- testdata/package_test/testpackagedata.go
+++
@@ -1,7 +0,0 @@
-package package_test
-
-type Method struct{}
-
-func (m *Method) Method1() string {
-	return ""
-}
--- testdata/package_test/testpackagedata1.go
+++
@@ -1,5 +0,0 @@
-package package_test
-
-func (m *Method) Method2() string {
-	return ""
-}
